9f18ca8c05ab429a35ba1876c557a76fb0ccc31b
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-01-11 00:35:30 +0100

    Add self-updater plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -348,8 +348,7 @@ async fn open_tar(path: &PathBuf) -> std::io::Result<async_tar::Archive<Compat<F
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet, install, create_patch])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
